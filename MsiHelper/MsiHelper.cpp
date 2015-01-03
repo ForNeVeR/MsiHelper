2463d9b3dcf037a49245d353de8f481b906d12c4
Friedrich von Never <friedrich@fornever.me>
2015-01-03 17:22:59 +0600

    Application structure.

--- MsiHelper/MsiHelper.cpp
+++ MsiHelper/MsiHelper.cpp
@@ -6,20 +6,25 @@
 #include <io.h>
 
 #include <array>
+#include <codecvt>
 #include <iostream>
+#include <locale>
+#include <string>
 #include <vector>
 
-int main(int argc, char **argv) {
-	_setmode(_fileno(stdout), _O_U16TEXT);
-	
-	std::wcout << L"Installed product codes:" << std::endl;
+void printUsage() {
+	std::wcout << "Usage: MsiHelper <command>\n"
+		"Where <command> is one of the following:\n"
+		"    list - lists all installed software." << std::endl;
+}
 
+void printList() {
 	std::array<wchar_t, 39> productCode;
 	std::vector<wchar_t> nameBuffer{ L'\0' };
 
 	for (DWORD i = 0; MsiEnumProductsW(i, productCode.data()) == ERROR_SUCCESS; ++i) {
 		std::wcout << productCode.data() << L" : ";
-		
+
 		DWORD bufferSize = nameBuffer.size();
 		switch (MsiGetProductInfoW(productCode.data(), INSTALLPROPERTY_INSTALLEDPRODUCTNAME, nameBuffer.data(), &bufferSize)) {
 		case ERROR_SUCCESS:
@@ -36,8 +41,27 @@ int main(int argc, char **argv) {
 			}
 		}
 
-		std::wcout << std::endl;		
+		std::wcout << std::endl;
+	}
+}
+
+int main(int argc, char **argv) {
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	if (argc < 2) {
+		printUsage();
 	}
 
-	std::wcout << L"End of installed product list." << std::endl;
+	auto command = std::string(argv[1]);
+	if (command == "list") {
+		printList();
+	} else {
+		// Little trick to convert the command name to wstring:
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		auto wcommand = converter.from_bytes(command);
+		std::wcout << L"Command " << wcommand << L" not recognized." << std::endl;
+
+		printUsage();
+	}
+
+	return 0;
 }
