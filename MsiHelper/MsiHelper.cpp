bcfa399414ccd16129d1a86b57a6f90ce74b3b39
Friedrich von Never <friedrich@fornever.me>
2015-01-05 23:37:43 +0600

    Add listComponents function.

--- MsiHelper/MsiHelper.cpp
+++ MsiHelper/MsiHelper.cpp
@@ -12,10 +12,13 @@
 #include <string>
 #include <vector>
 
+const int CodeBufferLength = 39; // 38 for GUID and 1 for terminating zero.
+
 void printUsage() {
 	std::wcout << "Usage: MsiHelper <command>\n"
 		"Where <command> is one of the following:\n"
 		"    list - lists all installed software.\n"
+		"    components <applicationName> - list application component info.\n"
 		"    path <applicationName> - get application installation path." << std::endl;
 }
 
@@ -36,13 +39,24 @@ std::wstring getProductProperty(const wchar_t *productCode, const wchar_t *prope
 	}
 }
 
+std::wstring getComponentPath(const wchar_t *productCode, const wchar_t *componentCode, std::vector<wchar_t> buffer) {
+	DWORD bufferSize = buffer.size();
+	auto result = MsiGetComponentPathW(productCode, componentCode, buffer.data(), &bufferSize);
+	if (bufferSize > buffer.size()) {
+		buffer.resize(bufferSize + 1);
+		result = MsiGetComponentPathW(productCode, componentCode, buffer.data(), &bufferSize);
+	}
+
+	return buffer.data();
+}
+
 std::wstring getProductProperty(const wchar_t *productCode, const wchar_t *property) {
 	std::vector<wchar_t> buffer{ L'\0' };
 	return getProductProperty(productCode, property, buffer);
 }
 
 void printList() {
-	std::array<wchar_t, 39> productCode;
+	std::array<wchar_t, CodeBufferLength> productCode;
 	std::vector<wchar_t> nameBuffer{ L'\0' };
 
 	for (DWORD i = 0; MsiEnumProductsW(i, productCode.data()) == ERROR_SUCCESS; ++i) {
@@ -56,7 +70,7 @@ void printList() {
 }
 
 std::wstring getProductCode(const std::wstring &productName) {
-	std::array<wchar_t, 39> productCode;
+	std::array<wchar_t, CodeBufferLength> productCode;
 	std::vector<wchar_t> nameBuffer{ L'\0' };
 
 	for (DWORD i = 0; MsiEnumProductsW(i, productCode.data()) == ERROR_SUCCESS; ++i) {
@@ -68,15 +82,34 @@ std::wstring getProductCode(const std::wstring &productName) {
 	return L"";
 }
 
+void listComponents(const std::wstring &applicationName) {
+	auto code = getProductCode(applicationName);
+	if (code.empty()) {
+		std::wcout << L"Error: application " << applicationName << L" not found" << std::endl;
+		return;
+	}
+
+	std::vector<wchar_t> buffer{ L'\0' };
+	std::array<wchar_t, CodeBufferLength> componentCode;
+	auto componentCodePtr = componentCode.data();
+	for (DWORD i = 0; MsiEnumComponentsW(i, componentCodePtr) == ERROR_SUCCESS; ++i) {
+		// TODO: It lists too many components, something's wrong.
+		std::wcout
+			<< componentCodePtr
+			<< " : "
+			<< getComponentPath(code.data(), componentCodePtr, buffer)
+			<< std::endl;
+	}
+}
+
 void printInstallationPath(const std::wstring &applicationName) {
 	auto code = getProductCode(applicationName);
 	if (code.empty()) {
 		std::wcout << L"Error: application " << applicationName << L" not found" << std::endl;
 	} else {
-		auto path = getProductProperty(code.data(), INSTALLPROPERTY_INSTALLLOCATION); // TODO: Still won't work. Need to query property of some product subcomponent?
+		auto path = getProductProperty(code.data(), INSTALLPROPERTY_INSTALLLOCATION); // TODO: Does not seem to work.
 		std::wcout << path << std::endl;
 	}
-	
 }
 
 int main(int argc, char **argv) {
@@ -90,7 +123,7 @@ int main(int argc, char **argv) {
 	auto command = std::string(argv[1]);
 	if (command == "list") {
 		printList();
-	} else if (command == "path") {
+	} else if (command == "components" || command == "path") {
 		if (argc < 3) {
 			std::wcout << "Invalid parameter count." << std::endl;
 			printUsage();
@@ -98,7 +131,12 @@ int main(int argc, char **argv) {
 		}
 
 		auto applicationName = converter.from_bytes(argv[2]);
-		printInstallationPath(applicationName);
+
+		if (command == "components") {
+			listComponents(applicationName);
+		} else {
+			printInstallationPath(applicationName);
+		}
 	} else {
 		auto wcommand = converter.from_bytes(command);
 		std::wcout << L"Command " << wcommand << L" not recognized." << std::endl;
